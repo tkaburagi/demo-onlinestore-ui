2a31c7db74c9113fd156bb23a19a857481ee5476
tkaburagi <tkaburagi@kabu-2.local>
2016-10-16 15:52:33 +0900

    initial

--- ci/tasks/smoke.sh
+++ ci/tasks/smoke.sh
@@ -5,7 +5,7 @@ sleep 10;
 i=0
 
 while [[ $i -ne 10 ]]; do
-	code=`curl -LI http://demo-onlinestore-ui-temp.local.pcfdev.io/login\?name\=tkaburagi -o /dev/null -w '%{http_code}\n' -s`;
+	code=`curl -LI http://demo-onlinestore-ui-temp.local.pcfdev.io/ -o /dev/null -w '%{http_code}\n' -s`;
 	if [[ ${code} = "200" ]]; then
 	    echo ${code};
 		echo "ok";
